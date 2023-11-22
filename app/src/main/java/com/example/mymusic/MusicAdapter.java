712a162fcbc3fc06eb4e0dbffce43352a777e63e
Nguyễn Thành Hưng <nguyenthanhhung.dev@gmail.com>
2023-11-23 04:27:15 +0700

    Fix mediaplayer went away with unhandled events

--- app/src/main/java/com/example/mymusic/MusicAdapter.java
+++ app/src/main/java/com/example/mymusic/MusicAdapter.java
@@ -8,6 +8,7 @@ import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -137,7 +138,7 @@ public class MusicAdapter extends RecyclerView.Adapter<MyViewHolder> {
                 playMusic(mediaPlayer);
             }
         } catch (IOException e) {
-
+             Toast.makeText(context,"Lỗi không phát được bài hát", Toast.LENGTH_LONG);
          }
         // Lưu lại holder cũ
         if (isPlaying || playingPosition != currentPosition) {
